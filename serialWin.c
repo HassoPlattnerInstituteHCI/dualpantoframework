8ad348bee1e5a768e919f5252f25f6fee107911c
Unknown <dajg1995@gmail.com>
2018-04-11 19:31:41 +0200

    added node code to serialWin.c

--- serialWin.c
+++ serialWin.c
@@ -19,8 +19,6 @@ BOOL setup(const char* ComPortName) {
 		printf("\n    Error! - Port %s can't be opened\n", ComPortName);
 		return FALSE;
 	}
-
-	/*------------------------------- Setting the Parameters for the SerialPort ------------------------------*/
 	
 	DCB dcbSerialParams = { 0 };                         // Initializing DCB structure
 	dcbSerialParams.DCBlength = sizeof(dcbSerialParams);
@@ -71,10 +69,7 @@ unsigned char receive() {
 	unsigned int index = 0;
 			
 	/*Status = WaitCommEvent(hComm, &dwEventMask, NULL); //Wait for the character to be received
-	if (Status == FALSE)
-	{
-		printf("\n    Error! in Setting WaitCommEvent()");
-	}*/
+	if (Status == FALSE) printf("\n    Error! in Setting WaitCommEvent()");*/
 	
 	while(index < strlen(packetStart)) {
 		Status = ReadFile(hComm, &TempChar, sizeof(TempChar), &NoBytesRead, NULL);
@@ -110,7 +105,7 @@ void sendPacket(unsigned char packetLength) {
         packetChecksum ^= packetBuffer[5+i];
     packetBuffer[5+packetLength] = packetChecksum;
 	
-	DWORD  dNoOfBytesWritten = 0;          // No of bytes written to the port
+	DWORD dNoOfBytesWritten = 0;          // No of bytes written to the port
 	
 	Status = WriteFile(hComm,               // Handle to the Serialport
 					   packetBuffer,            // Data to be written to the port 
@@ -120,6 +115,82 @@ void sendPacket(unsigned char packetLength) {
 	
 	if (Status == FALSE)
 		printf("\n\n   Error %d in Writing to Serial Port",GetLastError());
+}
+
+#ifdef NODE_GYP
+#include <node/node_api.h>
+
+napi_value nodeOpen(napi_env env, napi_callback_info info) {
+    size_t argc = 1;
+    napi_value argv[1];
+    if(napi_get_cb_info(env, info, &argc, argv, NULL, NULL) != napi_ok)
+        napi_throw_error(env, NULL, "Failed to parse arguments");
+    size_t length;
+    char buffer[64];
+    napi_get_value_string_utf8(env, argv[0], buffer, sizeof(buffer), &length);
+    if(!setup(buffer))
+        napi_throw_error(env, NULL, "open failed");
+    return NULL;
+}
+
+napi_value nodePoll(napi_env env, napi_callback_info info) {
+    napi_value result, value;
+    napi_create_array(env, &result);
+    size_t packet = 0;
+    while(true) {
+        unsigned char length = receive(), *underlyingBuffer;
+        if(length == 0)
+            break;
+        napi_create_buffer_copy(env, length, packetBuffer, (void**)&underlyingBuffer, &value);
+        napi_set_element(env, result, packet++, value);
+    }
+    return result;
+}
+
+napi_value nodeSend(napi_env env, napi_callback_info info) {
+    size_t argc = 1;
+    napi_value argv[1];
+    if(napi_get_cb_info(env, info, &argc, argv, NULL, NULL) != napi_ok)
+        napi_throw_error(env, NULL, "Failed to parse arguments");
+    unsigned char *payload;
+    size_t length;
+    napi_get_buffer_info(env, argv[0], (void**)&payload, &length);
+    memcpy(&packetBuffer[5], payload, length);
+    send(length);
+    return NULL;
+}
+
+napi_value nodeTest(napi_env env, napi_callback_info info){
+    printf("node test executing...\n");
+    // TODO do something meaningful here...
+    printf("test succeeded! :P \n");
+    return 0;
+}
+
+#define defFunc(name, ptr) \
+if(napi_create_function(env, NULL, 0, ptr, NULL, &fn) != napi_ok) \
+    napi_throw_error(env, NULL, "Unable to wrap native function"); \
+if(napi_set_named_property(env, exports, name, fn) != napi_ok) \
+    napi_throw_error(env, NULL, "Unable to populate exports");
+
+napi_value Init(napi_env env, napi_value exports) {
+    napi_value fn;
+    defFunc("open", nodeOpen);
+    defFunc("poll", nodePoll);
+    defFunc("send", nodeSend);
+    defFunc("test", nodeTest);
+    return exports;
+}
+
+NAPI_MODULE(NODE_GYP_MODULE_NAME, Init)
+
+#else
+#include <signal.h>
+#include <stdlib.h>
+
+void terminate(int signal) {
+	CloseHandle(hComm);//Closing the Serial Port
+    exit(0);
 }
 
 int main(int argc, char** argv){
@@ -134,18 +205,18 @@ int main(int argc, char** argv){
 	DWORD dwEventMask;                     // Event mask to trigger
 	char  SerialBuffer[256];               // Buffer Containing Rxed Data
 		
-    //signal(SIGINT, &terminate);
+    signal(SIGINT, &terminate);
     //fcntl(STDIN_FILENO, F_SETFL, O_NONBLOCK);
 	
 	char lineBuffer[255*3];
     unsigned char packetLength;
 	while(TRUE){
-		/*HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
+		HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
 		INPUT_RECORD input_records[50];
 		DWORD nb_read, nb_chars = 0, i;
-		PeekConsoleInput(hStdin, input_records, 50, &nb_read);/**/
+		PeekConsoleInput(hStdin, input_records, 50, &nb_read);
 		
-		if(/* > 0 && /**/fgets(lineBuffer, sizeof(lineBuffer), stdin)) {
+		if(nb_read > 0 && fgets(lineBuffer, sizeof(lineBuffer), stdin)) {
             packetLength = strlen(lineBuffer)/3;
             for(unsigned int byte, i = 0; i < packetLength; ++i) {
                 sscanf(&lineBuffer[i*3], "%02X", &byte);
@@ -162,7 +233,6 @@ int main(int argc, char** argv){
             fflush(stdout);
         }/**/
 	}
-
-	CloseHandle(hComm);//Closing the Serial Port
-	_getch();
 }
+
+#endif
