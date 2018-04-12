add4ee7c109f1f1551378fcd080eeb064ccc5445
Jotaro <jojojoe77@gmail.com>
2018-04-12 21:00:50 +0200

    added signal test, and readme instruction to change portname.

--- Utils/test.js
+++ Utils/test.js
@@ -3,28 +3,22 @@ var SerialPort = require('serialport');
 const serial = require('../build/Release/serial');
 var Vector = require('./Vector.js');
 serial.test();
-
-
-try{
-	serial.open("/dev/cu.usbmodem1411");
-} catch (e) {
-    console.log("ERROR: No serial port attached.");
-    if (DEBUG_WITHOUT_SERIAL)
-    {
-        console.log("DEBUG: DEBUG_WITHOUT_SERIAL is true, so running with SERIAL_EXISTS=false.");
-        SERIAL_EXISTS = false;
-    }
-}
-serialRecv();
 function connectArduino(port){
-    // try{
-    //     console.log('opening...');
-    //     serial.open("/dev/cu.usbmodem1411");
-    // } catch (e) {
-    //     console.error(e);
-    //     console.log("ERROR: No serial port attached.");
-    // }
-       
+    console.log(port);
+    try{
+        //MACUSERS
+        serial.open(port.replace("tty.", "cu."));
+        //OTHER USERS
+        // serial.open("YOUR_PORT_NAME");
+    } catch (e) {
+        console.log("ERROR: No serial port attached.");
+        console.log("ERROR: Your serial port name could be different. Please ask TA for help. Or, you can go /dev/ directory to find your arduino port. Change source code on ./Utils/test.js");
+        process.exit();
+    }
+    finally{
+        serialRecv();
+        console.log("TIPS: If you don't see numbers displayed on terminal, check the usb cable is connected on Native USB port on Arduino DUE.\nCTRL+c to abort.");
+    }
 }
 
 function serialRecv() {
@@ -41,30 +35,31 @@ function serialRecv() {
         values[i] = packet.readFloatLE(i*4);
     upperPanto = new Vector(values[0], values[1], values[2]);
     lowerPanto = new Vector(values[3], values[4], values[5]);
-    console.log(values[0]);
+    console.log(values[0] + " " + values[1] + " " + values[2] + " " + values[3] + " " + values[4] + " " + values[5]);
 }
 function getConnectedArduino(callback) {
-    // var arduinoport = "";
-    // SerialPort.list(function(err, ports) {
-    //   var allports = ports.length;
-    //   var count = 0;
-    //   var done = false
-    //   ports.forEach(function(port) {
-    //     // console.log(port);
-    //     count += 1;
-    //     pm = port['manufacturer'];
-    //     if (typeof pm !== 'undefined' && pm.includes('Arduino') ) {
-    //       arduinoport = port.comName.toString();
-    //       done = true;
-    //       callback(arduinoport);
-    //     //   console.log('your panto is found!');
-    //       return arduinoport;
-    //     }
-    //     if (count === allports && done === false) {
-    //        console.log('cant find arduino')
-    //     }
-    //   });
-    // });
+    var arduinoport = "";
+    SerialPort.list(function(err, ports) {
+      var allports = ports.length;
+      var count = 0;
+      var done = false
+      ports.forEach(function(port) {
+        // console.log(port);
+        count += 1;
+        pm = port['manufacturer'];
+        if (typeof pm !== 'undefined' && pm.includes('Arduino') ) {
+          arduinoport = port.comName.toString();
+          done = true;
+          callback(arduinoport);
+          console.log("your panto is found!");
+          return arduinoport;
+        }
+        if (count === allports && done === false) {
+           console.log('ERROR: cant find arduino...');
+           console.log("ERROR: Connect your Arduino Due's Native USB port. Make sure it's connected also to your PC.");
+        }
+      });
+    });
 }
 
 getConnectedArduino(connectArduino);
