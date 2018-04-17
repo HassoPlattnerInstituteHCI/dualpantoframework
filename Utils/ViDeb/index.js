844be20fd8f909361d581013d997b860e7d32f2a
Jotaro Shigeyama <jotaro.shigeyama@hpi.de>
2018-04-17 21:48:00 +0200

    added ViDeb stuff

--- Utils/ViDeb/index.js
+++ Utils/ViDeb/index.js
@@ -1,15 +1,89 @@
 var   express     = require('express'),
-      http    = require('http').Server(app),
-      socket  = require('socket.io'),
+      http    = require('http'),
+      fs = require('fs'),
+      WebSocketServer = require('websocket').server,
+      connections = new Set();
       path = require('path');
 
-var app = express();
-app.use(express.static(path.join(__dirname+'/static')));
+// var app = express();
+// app.use(express.static(path.join(__dirname+'/static')));
 
-app.get('/', function(req,res){
-    res.sendFile(__dirname + '/index.html');
+// app.get('/', function(req,res){
+//     res.sendFile(__dirname + '/index.html');
+// });
+
+// app.listen(8080, function(){
+//     console.log('listening on *:8080');
+// });
+const server = http.createServer((request, response) => {
+    let filePath = __dirname+((request.url == '/') ? '/index.html' :('/static'+request.url));
+    // let filepath = __dirname+'/index.html';
+    // let filePath = __dirname + '/static'
+    const extname = path.extname(filePath);
+    let contentType = 'text/plain';
+    switch(extname) {
+        case '.html':
+            contentType = 'text/html';
+            break;
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+    }
+
+    fs.readFile(filePath, function(error, content) {
+        if(error) {
+            if(error.code == 'ENOENT')
+                response.writeHead(404);
+            else
+                response.writeHead(500);
+        } else {
+            response.writeHead(200, {
+                'Content-Type': contentType
+            });
+            response.write(content);
+
+        }
+        response.end();
+    });
+}).on('clientError', (err, socket) => {
+    socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+}).listen(8080);
+
+
+wsServer = new WebSocketServer({
+    httpServer: server,
+    autoAcceptConnections: false
 });
-
-app.listen(8080, function(){
-    console.log('listening on *:8080');
+wsServer.on('request', function(request) {
+    var connection = request.accept();
+    connections.add(connection);
+    console.log((new Date()) + ' Connection accepted.');
+    connection.on('message', function(message) {
+        // console.log('< '+message.utf8Data);
+        // proc.stdin.write(message.utf8Data);
+        message = JSON.parse(message.utf8Data);
+        const data = new Buffer(9);
+        for(let i = 0; i < 2; ++i) {
+            data[0] = i;
+            data.writeInt32LE(message.angles[i], 1);
+            data.writeInt32LE(message.forces[i], 5);
+            serialSend(data);
+        }
+    });
+    connection.on('close', function(reasonCode, description) {
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+        connections.delete(connection);
+    });
 });
--- Utils/ViDeb/static/js/graphics.js
+++ Utils/ViDeb/static/js/graphics.js
@@ -1,1 +1,66 @@
-console.log("hello");
+
+var s = Snap('#svg');
+
+const height = s.node.clientHeight,
+       width = s.node.clientWidth;
+var config
+var style=
+{
+    "lineattr":{
+        "fill": "#fc0",
+        "stroke": "#000", 
+        "strokeWidth":1
+    }
+};
+$.getJSON("js/LP_PCB.json", function(json){
+    $.ajaxSetup({async:false});
+    config = json;
+    $.ajaxSetup({async:true});
+    draw(0,0);
+});
+
+
+function draw(_t1, _t2){
+    s.clear();
+    var t1 = _t1;
+    var t2 = _t2;
+
+    var panto = config.pantos.upper;
+    var ml = s.circle(panto.left.linkage.baseX, panto.left.linkage.baseY, 5).attr({fill:"red"});
+    var mr = s.circle(panto.right.linkage.baseX, panto.right.linkage.baseY, 5).attr({fill:"black"});
+    var il = s.line(panto.left.linkage.baseX, panto.left.linkage.baseY,
+                    panto.left.linkage.baseX+panto.left.linkage.innerLength * Math.cos(t1),
+                    panto.left.linkage.baseY+panto.left.linkage.innerLength * Math.sin(t1)).attr(style.lineattr);
+    var ir = s.line(panto.right.linkage.baseX, panto.right.linkage.baseY,
+                    panto.right.linkage.baseX+panto.right.linkage.innerLength * Math.cos(t2),
+                    panto.right.linkage.baseY+panto.right.linkage.innerLength * Math.sin(t2)).attr(style.lineattr);
+    
+    var a1 = panto.left.linkage.innerLength;
+    var a2 = panto.left.linkage.outerLength;
+    var a3 = panto.right.linkage.outerLength;
+    var a4 = panto.right.linkage.innerLength;
+
+    var P2 = new Vector(a1 * Math.cos(t1), a1 * Math.sin(t1));
+    var P4 = new Vector(a4 * Math.cos(t2) + Math.abs(panto.left.linkage.baseX)+panto.right.linkage.baseX, a4 * Math.sin(t2));
+
+
+    var P4_2 = P4.subtract(P2);
+    var P42  = P4_2.length();
+    var P2h = (a2*a2 - a3*a3 + P42*P42) / (2 * P42);
+    var Ph =  P2.add(P4_2.scaled(P2h / P42));
+    var P3h = Math.sqrt(a2*a2 - P2h*P2h);
+
+    var P3 = new Vector(Ph.x - P3h/P42 * (P4.y - P2.y),Ph.y + P3h/P42 * (P4.x-P2.x));
+
+    var ee = s.circle(P3.x + panto.left.linkage.baseX, P3.y, 5).attr({fill:"green"});
+    var e2 = s.circle(P2.x + panto.left.linkage.baseX, P2.y, 5).attr({fill:"blue"});
+    var e4 = s.circle(P4.x + panto.left.linkage.baseX, P4.y, 5).attr({fill:"orange"});
+    
+    var g = s.group(ml, mr, il, ir, ee, e2, e4)
+    g.transform('t 400, 100')
+}
+
+var slider = document.getElementById("myRange");
+slider.oninput = function(){
+    draw(slider.value/1000, 0);
+}
