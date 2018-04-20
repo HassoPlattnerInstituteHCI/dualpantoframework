36a4f09c8e0b4528eec6f2babdce90a288eb1910
Jotaro Shigeyama <jotaro.shigeyama@hpi.de>
2018-04-20 09:08:01 +0200

    deleted unused webclient

--- Framework.js
+++ Framework.js
@@ -4,8 +4,7 @@ const serial = require('./build/Release/serial'),
       Buffer = require('buffer').Buffer,
       Vector = require('./Vector.js'),
       SerialPort = require('serialport'),
-      EventEmitter = require('events').EventEmitter,
-      WebsocketClient = require('websocket').client;
+      EventEmitter = require('events').EventEmitter;
 
 class Broker extends EventEmitter {
     constructor() {
@@ -100,28 +99,3 @@ function autoDetectDevices() {
     });
 }
 autoDetectDevices();
-
-var WebSocketClient = require('websocket').client;
- 
-var client = new WebSocketClient();
- 
-client.on('connectFailed', function(error) {
-    console.log('Connect Error: ' + error.toString());
-});
- 
-client.on('connect', function(connection) {
-    console.log('WebSocket Client Connected');
-    connection.on('error', function(error) {
-        console.log("Connection Error: " + error.toString());
-    });
-    connection.on('close', function() {
-        console.log('echo-protocol Connection Closed');
-    });
-    connection.on('message', function(message) {
-        if (message.type === 'utf8') {
-            console.log("Received: '" + message.utf8Data + "'");
-        }
-    });
-});
- 
-client.connect('ws://localhost:8080/');
