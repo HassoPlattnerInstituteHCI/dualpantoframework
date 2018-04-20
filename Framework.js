195917de8e80a0402aa95f50b450aa0666c8a326
Jonas Bounama <jobounama@hotmail.de>
2018-04-20 08:36:13 +0200

    changed Framework with master knowledge and changed HelloStudentWorld to match HomefinderExample

--- Framework.js
+++ Framework.js
@@ -3,28 +3,36 @@
 const serial = require('./build/Release/serial'),
       Buffer = require('buffer').Buffer,
       Vector = require('./Vector.js'),
-      say = require('say-promise'),
-      PlaySound = require('play-sound')(),
-      TWEEN = require('@tweenjs/tween.js'),
-      co = require('co'),
       SerialPort = require('serialport'),
-      TWEEN_INTERVAL = 30;
+      EventEmitter = require('events').EventEmitter,
+      WebsocketClient = require('websocket').client;
 
-var tween_stack_counter = 0;
-const devices = new Map();
-module.exports.getDevices = function() {
-    return devices.values();
-};
+class Broker extends EventEmitter {
+    constructor() {
+        super();
+        this.devices = new Map();
+    }
 
-class Device {
+    getDevices() {
+        return this.devices.values();
+    }
+}
+
+const broker = new Broker();
+module.exports = broker;
+const ViDeb = require('./Utils/ViDeb/index');
+
+
+class Device extends EventEmitter {
     constructor(port) {
         if(process.platform == 'darwin') // macOS
             port = port.replace('/tty.', '/cu.');
         else if(process.platform == 'win32') // windows
             port = '//.//'+port;
-        if(devices.has(port))
-            return devices.get(port);
-        devices.set(port, this);
+        if(broker.devices.has(port))
+            return broker.devices.get(port);
+        super();
+        broker.devices.set(port, this);
         this.port = port;
         this.serial = serial.open(port);
         this.lastKnownPositions = [];
@@ -45,14 +53,12 @@ class Device {
         if(packet.length == 16)
             this.hardwareConfigHash = packet;
         else if(packet.length == 4*6) {
-            const values = [];
-            for(let i = 0; i < 6; ++i)
-                values[i] = packet.readFloatLE(i*4);
-            this.lastKnownPositions[0] = new Vector(values[0], values[1], values[2]);
-            this.lastKnownPositions[1] = new Vector(values[3], values[4], values[5]);
-            if(this.onHandleMoved) {
-                this.onHandleMoved(0, this.lastKnownPositions[0]);
-                this.onHandleMoved(1, this.lastKnownPositions[1]);
+            for(let i = 0; i < 2; ++i) {
+                const newPosition = new Vector(packet.readFloatLE(i*12), packet.readFloatLE(i*12+4), packet.readFloatLE(i*12+8));
+                if(this.lastKnownPositions[i] && newPosition.difference(this.lastKnownPositions[i]).length() <= 0.0)
+                    continue;
+                this.lastKnownPositions[i] = newPosition;
+                this.emit('handleMoved', i, this.lastKnownPositions[i]);
             }
         }
     }
@@ -70,6 +76,7 @@ class Device {
         packet.writeFloatLE(values[1], 5);
         packet.writeFloatLE(values[2], 9);
         this.send(packet);
+        this.emit('moveHandleTo', index, target);
     }
 
     movePantoTo(index, target, duration, interpolation_method=TWEEN.Easing.Quadratic.Out)
@@ -173,10 +180,11 @@ function animateTween() {
       setTimeout(animateTween, TWEEN_INTERVAL);
   }
 }
+}
 
 function serialRecv() {
     setImmediate(serialRecv);
-    for(const device of devices.values())
+    for(const device of broker.devices.values())
         device.poll();
 }
 serialRecv();
@@ -187,12 +195,36 @@ function autoDetectDevices() {
             console.error(err);
         else
             for(const port of ports)
-                if(port.manufacturer && port.manufacturer.includes('Arduino LLC'))
+                if(port.manufacturer && (port.manufacturer.includes('Arduino LLC') || port.manufacturer.includes('Atmel Corp. at91sam SAMBA bootloader'))){
+                    console.log('connected to : '+port.comName);
                     new Device(port.comName);
-        if(module.exports.onDevicesChanged)
-            module.exports.onDevicesChanged(devices.values());
+                }
+        broker.emit('devicesChanged', broker.devices.values());
     });
 }
 autoDetectDevices();
 
-module.exports = Device;
+var WebSocketClient = require('websocket').client;
+ 
+var client = new WebSocketClient();
+ 
+client.on('connectFailed', function(error) {
+    console.log('Connect Error: ' + error.toString());
+});
+ 
+client.on('connect', function(connection) {
+    console.log('WebSocket Client Connected');
+    connection.on('error', function(error) {
+        console.log("Connection Error: " + error.toString());
+    });
+    connection.on('close', function() {
+        console.log('echo-protocol Connection Closed');
+    });
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log("Received: '" + message.utf8Data + "'");
+        }
+    });
+});
+ 
+client.connect('ws://localhost:8080/');
