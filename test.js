ad130c51a468e06b744df3d1322b696783f2f211
Jotaro Shigeyama <jotaro.shigeyama@hpi.de>
2018-05-07 16:46:24 +0200

    lil' bit fix on test.js

--- test.js
+++ test.js
@@ -6,13 +6,13 @@ const Framework = require('./Framework.js'),
 Framework.on('devicesChanged', function(devices) {
     for(const device of devices) {
         device.on('handleMoved', function(index, position) {
-            console.log('handleMoved', index, position);
+            console.log(device.port, 'handleMoved', index, position);
         });
         device.moveHandleTo(0, new Vector(0,-80,0));
     }
 });
 
 // TODO: Test case termination
-// setTimeout(function() {
-//     process.exit(0);
-// }, 1000);
+setTimeout(function() {
+    process.exit(0);
+}, 1000);
