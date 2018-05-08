f18f9aabb1d4256d752b7ca2bae6a0f88f6c76b0
Jonas Bounama <jobounama@hotmail.de>
2018-05-08 16:44:17 +0200

    added build

---
+++ voice-command/build/build-release.js
@@ -0,0 +1,29 @@
+var os = require('os'),
+    fs = require('fs'),
+    spawn = require('child_process').spawn;
+
+
+var platform = os.platform();
+console.log(platform);
+if(platform === 'darwin') {
+    var child = spawn('xcodebuild', {
+    cwd: './voice-command/src/osx/voice-command/'
+    });
+}
+else if (platform == "win32") {
+    var msbuild = 'C:/Windows/Microsoft.NET/Framework64/v4.0.30319/msbuild.exe';
+    var child = spawn(msbuild, ['/p:Configuration=Release', 'voice-command.csproj', '/p:Platform=AnyCPU'], {
+    cwd: './voice-command/src/win/voice-command/'
+    });
+    child.stdout.on('data', function (data) { console.log('' + data); });
+    child.stderr.on('data', function (data) { console.log('' + data); });
+}
+else {
+    throw('Platform ' + platform + ' is not supported yet.');
+}
+child.on('close', function(code) {
+  if (code !== 0) {
+    console.log('Build process exited with code ' + code);
+    process.exit(1);
+  }
+});
