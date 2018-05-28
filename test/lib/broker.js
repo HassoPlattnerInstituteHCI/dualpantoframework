10a337416c8543e1124be5eec9f610a6592eec6f
Martin Michaelis <code@mgjm.de>
2018-05-28 21:43:22 +0200

    test: added initial tests for simple property and method existence

---
+++ test/lib/broker.js
@@ -0,0 +1,48 @@
+'use strict';
+
+/* eslint-disable global-require, max-nested-callbacks */
+
+const test = require('ava');
+
+const DualPantoFramework = require('../../Framework');
+const Broker = require('../../lib/broker');
+const VoiceInteraction = require('../../lib/voice-interaction');
+
+const broker = new Broker();
+
+test('constructor', t => {
+	t.is(DualPantoFramework.constructor, Broker);
+	t.is(broker.constructor, Broker);
+
+	t.deepEqual(broker.devices, new Map());
+	t.is(broker.disconnectTimeout, 5);
+	t.is(broker.voiceInteraction.constructor, VoiceInteraction);
+});
+
+const delay = () => new Promise(r => setTimeout(r, 10));
+test('run_script', async t => {
+	t.true(typeof DualPantoFramework.run_script === 'function');
+	await new Promise(resolve => {
+		DualPantoFramework.run_script([
+			() => delay(),
+			() => {
+				resolve();
+				return delay();
+			},
+		]);
+	});
+});
+
+test('waitMS', async t => {
+	t.true(typeof DualPantoFramework.waitMS === 'function');
+	t.true(await (async () => {
+		await DualPantoFramework.waitMS(10);
+		return true;
+	})());
+});
+
+test('getDevices', t => {
+	t.true(typeof DualPantoFramework.getDevices === 'function');
+	for(const device of DualPantoFramework.getDevices())
+		t.fail(`contains a device: ${device}`);
+});
