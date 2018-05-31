2a782f4030691bb7a91dcf93f1a1308d1e4100cb
Martin Michaelis <code@mgjm.de>
2018-05-31 12:21:56 +0200

    refactor: added deprecation warning to Framework.js

--- Framework.js
+++ Framework.js
@@ -1,4 +1,7 @@
 'use strict';
 
+const {deprecated} = require('./lib/util');
+deprecated("require('Framework.js')", "`const {Broker} = require('dualpantoframework'); const broker = new Broker();`");
+
 // export the dualpantoframework from the lib directory
 module.exports = require('./lib/dualpantoframework');
