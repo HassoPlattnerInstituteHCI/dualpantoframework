a90e3238ef907af71a9f3e527262cb03384588e8
Lukas Wagner <lukas.wagner@student.hpi.de>
2019-02-25 15:33:32 +0100

    minor fixes

--- posTest.js
+++ posTest.js
@@ -7,10 +7,14 @@ Framework.on('devicesChanged', function(devices, attached, detached) {
   console.log(`devices: ${devices.size}, attached: ${attached.size}, detached: ${detached.size}`);
   for(const device of devices) {
       if(device){
-        device.createObstacle([new Vector(-20, -10, 0), new Vector(20, -10, 0), new Vector(20, -20, 0), new Vector(-20, -20, 0)]);
         device.on('handleMoved', function(index, position){
           console.log('index: ', index, ' position: ', position);
         });
+        device.createObstacle([
+          new Vector(50, -200, 0),
+          new Vector(-50, -200, 0),
+          new Vector(-50, -80, 0),
+          new Vector(50, -80, 0)]);
       }
   }
 });
