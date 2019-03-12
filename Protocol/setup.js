6512f6883f07b96ac493f1520b83fc85239e758d
Lukas Wagner <lukas.wagner@student.hpi.de>
2019-03-12 16:39:38 +0100

    fix serial setup

--- Protocol/setup.js
+++ Protocol/setup.js
@@ -1,14 +1,10 @@
 const fs = require('fs');
 
-const targets = ["ESP32"];
-
-for (const target of targets) {
-  fs.copyFile('./Protocol/protocol.hpp', './Firmware/' + target + '/protocol.hpp', (err) => {
-    if (err) throw err;
-    console.log('copied protocol.hpp to ' + target);
-  });
-  fs.copyFile('./Protocol/protocol.cpp', './Firmware/' + target + '/protocol.cpp', (err) => {
-    if (err) throw err;
-    console.log('copied protocol.cpp to ' + target);
-  });
-}
+fs.copyFile('./Protocol/protocol.hpp', './Firmware/include/protocol.hpp', (err) => {
+  if (err) throw err;
+  console.log('copied protocol.hpp');
+});
+fs.copyFile('./Protocol/protocol.cpp', './Firmware/lib/protocol.cpp', (err) => {
+  if (err) throw err;
+  console.log('copied protocol.cpp');
+});
