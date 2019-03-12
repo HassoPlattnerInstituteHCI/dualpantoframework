43ef4ea4810825dfb6bfda414dbe194d8ff81a83
Lukas Wagner <lukas.wagner@student.hpi.de>
2019-03-12 16:54:37 +0100

    listened to linter

--- Protocol/setup.js
+++ Protocol/setup.js
@@ -1,10 +1,14 @@
 const fs = require('fs');
 
-fs.copyFile('./Protocol/protocol.hpp', './Firmware/include/protocol.hpp', (err) => {
-  if (err) throw err;
-  console.log('copied protocol.hpp');
-});
-fs.copyFile('./Protocol/protocol.cpp', './Firmware/lib/protocol.cpp', (err) => {
-  if (err) throw err;
-  console.log('copied protocol.cpp');
-});
+fs.copyFile(
+    './Protocol/protocol.hpp', './Firmware/include/protocol.hpp',
+    (err) => {
+      if (err) throw err;
+      console.log('copied protocol.hpp');
+    });
+fs.copyFile(
+    './Protocol/protocol.cpp', './Firmware/lib/protocol.cpp',
+    (err) => {
+      if (err) throw err;
+      console.log('copied protocol.cpp');
+    });
