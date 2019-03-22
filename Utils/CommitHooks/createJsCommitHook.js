8bd055b7908a1412e062b3e092bfb55a98f2e848
Jonas Bounama <jobounama@hotmail.de>
2019-03-22 15:04:38 +0100

    updated script output

--- Utils/CommitHooks/createJsCommitHook.js
+++ Utils/CommitHooks/createJsCommitHook.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 const fs = require('fs');
 const script = "#!/bin/sh\n\
 \n\
@@ -10,7 +11,7 @@ fi\n\
 \n\
 PASS=true\n\
 \n\
-printf \"\\nValidating Javascriptcode:\\n\"\n\
+printf \"\\nRunning ESLint for JS:\\n\"\n\
 \n\
 # Check for eslint\n\
 if [[ ! -x \"$ESLINT\" ]]; then\n\
@@ -23,14 +24,15 @@ do\n\
   \"$ESLINT\" \"$FILE\"\n\
 \n\
   if [[ \"$?\" == 0 ]]; then\n\
-    printf \"\\t\\033[32mESLint Passed: $FILE\\033[0m\"\n\
+    printf \"\\033[32mESLint Passed: $FILE\\033[0m\"\n\
   else\n\
-    printf \"\\t\\033[41mESLint Failed: $FILE\\033[0m\"\n\
+    printf \"\\033[41mESLint Failed: $FILE\\033[0m\"\n\
     PASS=false\n\
   fi\n\
+  printf \"\\t\"\n\
 done\n\
 \n\
-printf \"\\nJavascriptcode validation completed!\\n\"\n\
+printf \"\\nESLint complete!\\n\"\n\
 \n\
 if ! $PASS; then\n\
   printf \"\\033[41mCOMMIT FAILED:\\033[0m Your commit contains files that should pass ESLint but do not. Please fix the ESLint errors and try again.\\n\"\n\
@@ -39,6 +41,6 @@ else\n\
   printf \"\\033[42mCOMMIT SUCCEEDED\\033[0m\\n\"\n\
 fi\n\
 \n\
-exit $?"
+exit $?";
 
 fs.writeFileSync('./.git/hooks/pre-commit', script);
