0ab4a31997aa62ebd0ab8d704c8fc7638d709a1b
Unknown <dajg1995@gmail.com>
2019-04-05 14:11:31 +0200

    removed debug code

--- examples/precisionTest/roomFractal.js
+++ examples/precisionTest/roomFractal.js
@@ -3,134 +3,125 @@
 const Framework = require('./../../');
 const {Vector} = Framework;
 
+/**
+ *
+ * @param {Vector[]} path - array where the points will be added
+ * @param {boolean} leftToBottom - true if the room should have a door at the
+ * left and bottom; false if the doors should be at the top and right
+ * @param {number} startX - x-position to start the room generation with
+ * @param {number} startY - y-position to start the room generation with
+ * @param {number} wallLength - width and height of the room
+ * @param {number} steps - how many rooms should be generated recursively
+ */
 function generateRoom(
     path,
     leftToBottom,
     startX,
     startY,
     wallLength,
-    steps)
-{
-    if(steps < 1) return;
+    steps) {
+  if (steps < 1) return;
 
-    let l3 = wallLength / 3;
-    let l4 = wallLength / 4;
+  const l3 = wallLength / 3;
+  const l4 = wallLength / 4;
 
-    if(leftToBottom)
-    {
-        path.push(new Vector(startX, startY)); // next to entrance
-        path.push(new Vector(startX, startY - l4)); // corner bottom left
-        path.push(new Vector(startX + l3, startY - l4)); // next to exit
+  if (leftToBottom) {
+    path.push(new Vector(startX, startY)); // next to entrance
+    path.push(new Vector(startX, startY - l4)); // corner bottom left
+    path.push(new Vector(startX + l3, startY - l4)); // next to exit
 
-        generateRoom(path, !leftToBottom, startX + l3, startY - l4 - l3, l3 * 2, steps-1);
+    generateRoom(
+        path,
+        !leftToBottom,
+        startX + l3,
+        startY - l4 - l3,
+        l3 * 2,
+        steps-1
+    );
 
-        path.push(new Vector(startX + l3 * 2, startY - l4)); // next to exit
-        path.push(new Vector(startX + wallLength, startY - l4)); // corner bottom right
-        path.push(new Vector(startX + wallLength, startY - l4 + wallLength)); // corner top right
-        path.push(new Vector(startX, startY - l4 + wallLength)); // corner top left
-        path.push(new Vector(startX, startY + 2 * l4)); // next to entrance
-    }
-    else
-    {
-        path.push(new Vector(startX, startY)); // next to entrance
-        path.push(new Vector(startX - l4, startY)); // corner top left
-        path.push(new Vector(startX - l4, startY - wallLength)); // corner bottom left
-        path.push(new Vector(startX - l4 + wallLength, startY - wallLength)); // corner bottom right
-        path.push(new Vector(startX - l4 + wallLength, startY - wallLength + l3)); // next to exit
+    // next to exit
+    path.push(new Vector(startX + l3 * 2, startY - l4));
+    // corner bottom right
+    path.push(new Vector(startX + wallLength, startY - l4));
+    // corner top right
+    path.push(new Vector(startX + wallLength, startY - l4 + wallLength));
+    // corner top left
+    path.push(new Vector(startX, startY - l4 + wallLength));
+    // next to entrance
+    path.push(new Vector(startX, startY + 2 * l4));
+  } else {
+    // next to entrance
+    path.push(new Vector(startX, startY));
+    // corner top left
+    path.push(new Vector(startX - l4, startY));
+    // corner bottom left
+    path.push(new Vector(startX - l4, startY - wallLength));
+    // corner bottom right
+    path.push(new Vector(startX - l4 + wallLength, startY - wallLength));
+    // next to exit
+    path.push(new Vector(startX - l4 + wallLength, startY - wallLength + l3));
 
-        generateRoom(path, !leftToBottom, startX - l4 + wallLength + l3, startY - wallLength + l3, l3 * 2, steps-1);
+    generateRoom(
+        path,
+        !leftToBottom,
+        startX - l4 + wallLength + l3,
+        startY - wallLength + l3,
+        l3 * 2,
+        steps-1
+    );
 
-        path.push(new Vector(startX - l4 + wallLength, startY - l3)); // next to exit
-        path.push(new Vector(startX - l4 + wallLength, startY)); // corner top right
-        path.push(new Vector(startX + 2 * l4, startY)); // next to entrance
-    }
-    
-}
-
-function generateRooms(path)
-{
-    generateRoom(path, false, -30, -80, 50, 4);
-    console.log("poligon");
-    
+    // next to exit
+    path.push(new Vector(startX - l4 + wallLength, startY - l3));
+    // corner top right
+    path.push(new Vector(startX - l4 + wallLength, startY));
+    // next to entrance
+    path.push(new Vector(startX + 2 * l4, startY));
+  }
 }
 
 Framework.on('devicesChanged', function(devices, attached, detached) {
+  // cant break in template string
+  // eslint-disable-next-line max-len
   console.log(`devices: ${devices.size}, attached: ${attached.size}, detached: ${detached.size}`);
   for (const device of devices) {
     if (device) {
-      device.on('handleMoved', function(index, position) {
-        console.log('index: ', index, ' position: ', position);
-      });
       Framework.run_script([
-            () => device.movePantoTo(0, new Vector(-20, -100, NaN), 30),
-            //() => device.moveHandleTo(0, new Vector(-20, -100, NaN)),
-            () => {
-                return new Promise(resolve => {
-                    setTimeout( () => {
-                        let path = [];
-                        generateRooms(path);
-                        let minX = 10000;
-                        let maxX = -10000;
-                        let minY = 10000;
-                        let maxY = -10000;
-                        console.log("(%s,%s) (%s,%s)", minX.toFixed(1), minY.toFixed(1), maxX.toFixed(1), maxY.toFixed(1));
-                        
-                        path = path.slice(0, 31);
-                        console.log(path);
-                        let path2 = [new Vector(-50, -80), new Vector(50, -80)];
-                        console.log(path2);
-                        //path = path2;
-                        
-                        let out = "";
-                        for(let p of path)
-                        {
-                            //console.log("%s|%s", p.x.toFixed(2), p.y.toFixed(2));
-                            out += p.x.toFixed(2) + "|" +  p.y.toFixed(2) + " ";
-                            //console.log(p);
-                            minX = Math.min(minX, p.x);
-                            maxX = Math.max(maxX, p.x);
-                            minY = Math.min(minY, p.y);
-                            maxY = Math.max(maxY, p.y);
-                            //p.r = 0;
-                        }
-                        console.log(out);
-                
-                        device.createObstacle(path);
-                    }, 3000)
-                });
-            },/**/
-      ]);
-        // setTimeout(() => {
-        //     let path = [];
-        //     generateRooms(path);
-        //     let minX = 10000;
-        //     let maxX = -10000;
-        //     let minY = 10000;
-        //     let maxY = -10000;
-        //     console.log("(%s,%s) (%s,%s)", minX.toFixed(1), minY.toFixed(1), maxX.toFixed(1), maxY.toFixed(1));
-            
-        //     path = path.slice(0, 31);
-        //     console.log(path);
-        //     let path2 = [new Vector(-50, -80), new Vector(50, -80)];
-        //     console.log(path2);
-        //     //path = path2;
-            
-        //     let out = "";
-        //     for(let p of path)
-        //     {
-        //         //console.log("%s|%s", p.x.toFixed(2), p.y.toFixed(2));
-        //         out += p.x.toFixed(2) + "|" +  p.y.toFixed(2) + " ";
-        //         //console.log(p);
-        //         minX = Math.min(minX, p.x);
-        //         maxX = Math.max(maxX, p.x);
-        //         minY = Math.min(minY, p.y);
-        //         maxY = Math.max(maxY, p.y);
-        //         //p.r = 0;
-        //     }
-        //     console.log(out);
+        // () => device.movePantoTo(0, new Vector(-20, -100, NaN), 30),
+        () => {
+          return new Promise((resolve) => {
+            setTimeout( () => {
+              let path = [];
+              generateRoom(path, false, -30, -80, 50, 4);
+              let minX = 10000;
+              let maxX = -10000;
+              let minY = 10000;
+              let maxY = -10000;
+              console.log(
+                  '(%s,%s) (%s,%s)',
+                  minX.toFixed(1),
+                  minY.toFixed(1),
+                  maxX.toFixed(1),
+                  maxY.toFixed(1)
+              );
 
-        //     device.createObstacle(path);
-        // }, 1000);
+              path = path.slice(0, 31);
+              console.log(path);
+
+              let out = '';
+              for (const p of path) {
+                out += p.x.toFixed(2) + '|' + p.y.toFixed(2) + ' ';
+                minX = Math.min(minX, p.x);
+                maxX = Math.max(maxX, p.x);
+                minY = Math.min(minY, p.y);
+                maxY = Math.max(maxY, p.y);
+              }
+              console.log(out);
+
+              device.createObstacle(path);
+            }, 3000);
+          });
+        }
+      ]);
     }
   }
 });
