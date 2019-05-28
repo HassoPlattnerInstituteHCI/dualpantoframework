aa0e4dc752dd831eac80b3c8f2db91e5a08121eb
Lukas Wagner <lukas.wagner@student.hpi.de>
2019-05-28 15:14:37 +0200

    some docs and stuff

--- examples/hapticObject/collider.js
+++ examples/hapticObject/collider.js
@@ -1,27 +1,42 @@
 'use strict';
 
 const Framework = require('../..');
-const {Vector, Components} = Framework;
+const {Vector, Components, Broker, HapticObject} = Framework;
 const {
   Mesh,
   MeshCollider,
   BoxCollider} = Components;
 
-Framework.on('devicesChanged', function(devices, attached, detached) {
-  for (const device of devices) {
-    if (device) {
-      // adding a simple box collider around (-100|-100)
-      const leftHapticObject = device.addHapticObject(new Vector(-100, -100));
-      leftHapticObject.addComponent(new BoxCollider(new Vector(100, 100)));
-      // adding a mesh collider around (100|-100) - in this case, also a box
-      const rightHapticObject = device.addHapticObject(new Vector(100, -100));
-      const mesh = rightHapticObject.addComponent(
-          new Mesh([
-            new Vector(-50, -50, 0),
-            new Vector(-50, 50, 0),
-            new Vector(50, 50, 0),
-            new Vector(50, -50, 0)]));
-      rightHapticObject.addComponent(new MeshCollider(mesh));
-    }
-  }
-});
+/**
+ * @typedef {import('../../lib/device')} Device
+ */
+
+Broker.on('devicesChanged',
+    /**
+     * @description React to connected or disconnected devices.
+     * @param {Device[]} devices - Connected devices.
+     * @param {Device[]} attached - Newly connected devices.
+     * @param {Device[]} detached - Disconencted devices.
+     */
+    function(devices, attached, detached) {
+      for (const device of devices) {
+        if (device) {
+          // adding a simple box collider around (-100|-100)
+          const leftHapticObject =
+            device.addHapticObject(new Vector(-100, -100));
+          leftHapticObject.addComponent(new BoxCollider(new Vector(100, 100)));
+          // adding a mesh collider around (100|-100) - in this case, also a box
+          const rightHapticObject =
+            device.addHapticObject(new Vector(100, -100));
+          const mesh = rightHapticObject.addComponent(
+              new Mesh([
+                new Vector(-50, -50, 0),
+                new Vector(-50, 50, 0),
+                new Vector(50, 50, 0),
+                new Vector(50, -50, 0)]));
+          rightHapticObject.addComponent(new MeshCollider(mesh));
+          const ho = new HapticObject(new Vector());
+          ho.a;
+        }
+      }
+    });
