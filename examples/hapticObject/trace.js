516531e141d6ca2e19a2848fa74b71777cfe6b87
Jonas Bounama <jobounama@hotmail.de>
2019-07-02 10:06:53 +0200

    small fixes

--- examples/hapticObject/trace.js
+++ examples/hapticObject/trace.js
@@ -19,10 +19,10 @@ Broker.on('devicesChanged', function(devices) {
 
 const start = () => {
   open('http://localhost:8080/map.html');
-  setTimeout(doStuff, 3000);
+  setTimeout(startTrace, 3000);
 };
 
-const doStuff = () => {
+const startTrace = () => {
   const hapticMeshObject0 = device.addHapticObject(new Vector(-89.878613,
       -76.828147));
   const mesh0 = hapticMeshObject0.addComponent(new Mesh([
@@ -30,9 +30,8 @@ const doStuff = () => {
     new Vector(21.762363000000008, -38.966543),
     new Vector(2.5083079999999995, -49.71970300000001),
     new Vector(-19.254056000000006, -10.753169999999997)
-  ]));// path6206
+  ]));
   const collider = hapticMeshObject0.addComponent(new MeshCollider(mesh0));
-  //
   const leftHapticObject = device.addHapticObject(
       new Vector(0, -150));
   const forcefield = leftHapticObject.addComponent(
