37817c784b8199f3acd618c556fe2d499633f7d6
Lukas Wagner <lukas.wagner@student.hpi.de>
2019-10-15 14:55:18 +0200

    could it be? a working unity prototype?

--- utils/serial/include/libInterface.hpp
+++ utils/serial/include/libInterface.hpp
@@ -5,14 +5,31 @@
 #include "serial.hpp"
 #include "serial_export.hpp"
 
+// class stuff
+
 class CppLib : DPSerial
 {
 public:
     static uint32_t getRevision();
+    static uint64_t open(char* port);
+    static void setActiveHandle(uint64_t handle);
+    static void close();
+    static void poll();
+    static void sendSyncAck();
+    static void sendHeartbeatAck();
 };
 
+// handlers
+
+typedef void (*syncHandler_t)(uint64_t);
+extern syncHandler_t syncHandler;
+typedef void (*heartbeatHandler_t)(uint64_t);
+extern heartbeatHandler_t heartbeatHandler;
+typedef void (*positionHandler_t)(uint64_t, double*);
+extern positionHandler_t positionHandler;
 typedef void (*loggingHandler_t)(char*);
 extern loggingHandler_t loggingHandler;
+void log(char* msg);
 
 // can't export any member functions, not even static ones
 // thus we'll have to add wrappers for everything
@@ -20,5 +37,13 @@ extern loggingHandler_t loggingHandler;
 extern "C"
 {
     uint32_t SERIAL_EXPORT GetRevision();
+    void SERIAL_EXPORT SetSyncHandler(syncHandler_t handler);
+    void SERIAL_EXPORT SetHeartbeatHandler(heartbeatHandler_t handler);
+    void SERIAL_EXPORT SetPositionHandler(positionHandler_t handler);
     void SERIAL_EXPORT SetLoggingHandler(loggingHandler_t handler);
+    uint64_t SERIAL_EXPORT Open(char* port);
+    void SERIAL_EXPORT Close(uint64_t handle);
+    void SERIAL_EXPORT Poll(uint64_t handle);
+    void SERIAL_EXPORT SendSyncAck(uint64_t handle);
+    void SERIAL_EXPORT SendHeartbeatAck(uint64_t handle);
 };
--- utils/serial/src/cppLib/lib.cpp
+++ utils/serial/src/cppLib/lib.cpp
@@ -2,22 +2,208 @@
 
 #include <iostream>
 
+// class stuff
+
 uint32_t CppLib::getRevision()
 {
     return c_revision;
 }
 
+uint64_t CppLib::open(char* port)
+{
+    if(!setup(port))
+    {
+        log("Open failed");
+        return 0;
+    }
+    log("Open successfull");
+    return (uint64_t) s_handle;
+}
+
+void CppLib::setActiveHandle(uint64_t handle)
+{
+    s_handle = (void*) handle;
+}
+
+void CppLib::close()
+{
+    tearDown();
+}
+
+void CppLib::poll()
+{
+    bool receivedSync = false;
+    bool receivedHeartbeat = false;
+    bool receivedPosition = false;
+    double positionCoords[2 * 5];
+
+    while (getAvailableByteCount(s_handle))
+    {
+        receivePacket();
+
+        if (s_header.PayloadSize > c_maxPayloadSize)
+        {
+            continue;
+        }
+
+        uint16_t offset = 0;
+        switch (s_header.MessageType)
+        {
+        case SYNC:
+        {
+            auto receivedRevision = DPSerial::receiveUInt32(offset);
+            if (receivedRevision == c_revision)
+            {
+                receivedSync = true;
+            }
+            else
+            {
+                log("Revision id not matching");
+            }
+            break;
+        }
+        case HEARTBEAT:
+            receivedHeartbeat = true;
+            break;
+        case POSITION:
+            receivedPosition = true;
+            while (offset < s_header.PayloadSize)
+            {
+                uint8_t index = offset / 4;
+                positionCoords[index] = DPSerial::receiveFloat(offset);
+            }
+            break;
+        case DEBUG_LOG:
+            log((char*)s_packetBuffer);
+            break;
+        default:
+            break;
+        }
+    }
+
+    if(receivedSync)
+    {
+        if(syncHandler == nullptr)
+        {
+            log("Received sync, but handler not set up");
+        }
+        else
+        {
+            syncHandler((uint64_t)s_handle);
+        }
+    }
+
+    if(receivedHeartbeat)
+    {
+        if(heartbeatHandler == nullptr)
+        {
+            log("Received heartbeat, but handler not set up");
+        }
+        else
+        {
+            heartbeatHandler((uint64_t)s_handle);
+        }
+    }
+
+    if (receivedPosition)
+    {
+        if(positionHandler == nullptr)
+        {
+            log("Received position, but handler not set up");
+        }
+        else
+        {
+            positionHandler((uint64_t)s_handle, positionCoords);
+        }
+    }
+}
+
+void CppLib::sendSyncAck ()
+{
+    s_header.MessageType = SYNC_ACK;
+    s_header.PayloadSize = 0;
+    sendPacket();
+}
+
+void CppLib::sendHeartbeatAck ()
+{
+    s_header.MessageType = HEARTBEAT_ACK;
+    s_header.PayloadSize = 0;
+    sendPacket();
+}
+
+// handlers
+
+syncHandler_t syncHandler;
+heartbeatHandler_t heartbeatHandler;
+positionHandler_t positionHandler;
 loggingHandler_t loggingHandler;
+
+void log (char* msg)
+{
+    if(loggingHandler != nullptr)
+    {
+        loggingHandler(msg);
+    }
+}
 
 // can't export any member functions, not even static ones
 // thus we'll have to add wrappers for everything
 
-#define ALIAS(alias, func) alias { return CppLib::func; }
+uint32_t GetRevision()
+{
+    return CppLib::getRevision();
+}
 
-ALIAS(uint32_t GetRevision(), getRevision())
+void SERIAL_EXPORT SetSyncHandler(syncHandler_t handler)
+{
+    syncHandler = handler;
+    log("Sync handler set");
+}
+
+void SERIAL_EXPORT SetHeartbeatHandler(heartbeatHandler_t handler)
+{
+    heartbeatHandler = handler;
+    log("Heartbeat handler set");
+}
+
+void SERIAL_EXPORT SetPositionHandler(positionHandler_t handler)
+{
+    positionHandler = handler;
+    log("Position handler set");
+}
 
 void SERIAL_EXPORT SetLoggingHandler(loggingHandler_t handler)
 {
     loggingHandler = handler;
-    loggingHandler("Ground control to major Tom");
+    loggingHandler("Logging from plugin is enabled");
+}
+
+uint64_t SERIAL_EXPORT Open(char* port)
+{
+    return CppLib::open(port);
+}
+
+void SERIAL_EXPORT Close(uint64_t handle)
+{
+    CppLib::setActiveHandle(handle);
+    CppLib::close();
+}
+
+void SERIAL_EXPORT Poll(uint64_t handle)
+{
+    CppLib::setActiveHandle(handle);
+    CppLib::poll();
+}
+
+void SERIAL_EXPORT SendSyncAck(uint64_t handle)
+{
+    CppLib::setActiveHandle(handle);
+    CppLib::sendSyncAck();
+}
+
+void SERIAL_EXPORT SendHeartbeatAck(uint64_t handle)
+{
+    CppLib::setActiveHandle(handle);
+    CppLib::sendHeartbeatAck();
 }
