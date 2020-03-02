bc476a2d5d2908b809420bcfa67730fe9c61058b
Jotaro <jojojoe77@gmail.com>
2020-03-02 13:29:17 +0100

    Fixed function name

--- examples/basic/move.js
+++ examples/basic/move.js
@@ -5,7 +5,7 @@ const {Broker, Vector} = require('../..');
 Broker.on('devicesChanged', function(devices, attached, detached) {
   for (const device of devices) {
     if (device) {
-      Broker.run_script([
+      Broker.runScript([
         () => device.movePantoTo(0, new Vector(-100, -100), 50),
         () => Broker.waitMS(3000),
         () => device.movePantoTo(0, new Vector(0, -100), 50),
