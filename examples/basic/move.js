3a321748f7a2600a0aa3bbb8c83f5687f7dfc7e4
Jotaro <jojojoe77@gmail.com>
2020-05-29 20:22:52 +0200

    update move.js to test all movement

--- examples/basic/move.js
+++ examples/basic/move.js
@@ -6,21 +6,17 @@ Broker.on('devicesChanged', function(devices, attached, detached) {
   for (const device of devices) {
     if (device) {
       Broker.runScript([
-        
-        () => device.movePantoTo(0, new Vector(-48, -92), 50),
-        /*
+        () => device.movePantoTo(0, new Vector(-20, -50), 50),
         () => Broker.waitMS(2000),
-        () => device.movePantoTo(1, new Vector(30, -92), 50),
-
-        
-        () => device.movePantoTo(0, new Vector(50, -100), 50),
-        () => Broker.waitMS(2000),
-        () => device.movePantoTo(0, new Vector(0, -100), 50),
-        /*
-        () => Broker.waitMS(3000),
         () => device.movePantoTo(0, new Vector(20, -50), 50),
-        () => Broker.waitMS(3000),
-*/
+        () => Broker.waitMS(2000),
+        () => device.movePantoTo(1, new Vector(-20, -50), 50),
+        () => Broker.waitMS(2000),
+        () => device.movePantoTo(1, new Vector(20, -50), 50),
+        () => Broker.waitMS(2000),
+        () => device.movePantoTo(0, new Vector(NaN, NaN), 50),
+        () => Broker.waitMS(2000),
+        () => device.movePantoTo(1, new Vector(NaN, NaN), 50)
       ]);
     }
   }
