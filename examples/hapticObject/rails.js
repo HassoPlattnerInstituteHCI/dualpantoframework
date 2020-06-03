991fcd81e3304b896d04027d13b0cf58e0dc9ed1
Julius Rudolph <julius.rudolph@student.hpi.de>
2020-06-03 14:45:29 +0200

    fix linting errors

--- examples/hapticObject/rails.js
+++ examples/hapticObject/rails.js
@@ -2,11 +2,17 @@
 const DualPantoFramework = require('../..');
 const {Vector, Broker, Components} = DualPantoFramework;
 const {BoxForcefield} = Components;
-
+const {
+  BoxCollider} = Components;
 /**
  * @type {import('../../lib/device')}
  */
 let device;
+
+const stdin = process.stdin;
+stdin.setRawMode(true);
+stdin.resume();
+stdin.setEncoding('utf8');
 
 Broker.on('devicesChanged', function(devices) {
   for (const newdevice of devices) {
@@ -21,86 +27,187 @@ function getBaseLog(x, y) {
   return Math.log(y) / Math.log(x);
 }
 
+// TODO: move this into the vector class
+function pDistance(x, y, x1, y1, x2, y2) {
+  const A = x - x1;
+  const B = y - y1;
+  const C = x2 - x1;
+  const D = y2 - y1;
+
+  const dot = A * C + B * D;
+  const lenSq = C * C + D * D;
+  let param = -1;
+  if (lenSq != 0) {
+    param = dot / lenSq;
+  }
+
+  let xx; let yy;
+
+  if (param < 0) {
+    xx = x1;
+    yy = y1;
+  } else if (param > 1) {
+    xx = x2;
+    yy = y2;
+  } else {
+    xx = x1 + param * C;
+    yy = y1 + param * D;
+  }
+
+  const dx = x - xx;
+  const dy = y - yy;
+  return Math.sqrt(dx * dx + dy * dy);
+}
+
+// behaves like a rail which sucks the handle in
+const negativeLogarithmicRails = function(distToRail, maxForce, range) {
+  let force = 0;
+  const multiplicator = distToRail > 0 ? -1 : 1;
+  force = (-getBaseLog(range, Math.abs(distToRail)) + 1) * maxForce
+    * multiplicator;
+  if (distToRail > -1 && distToRail < 1) {
+    force = maxForce * multiplicator;
+    // force = 0;
+  }
+  return force;
+};
+
 
 // behave like a bump with a sharp edge at the position of the rail
-const negativeLogarithmicRails = function(x, maxForce, range) {
+const negativeLogarithmicBump = function(distToRail, maxForce, range) {
   let force = 0;
-  if (x < 0 && x >= -range || x > 0 && x <= range) {
-    const multiplicator = x < 0 ? -1 : 1;
-    force = (-getBaseLog(10, x * multiplicator) + 1) * maxForce * multiplicator;
-    if (x > -1 && x < 1) {
-      force = maxForce * multiplicator;
-      // force = 0;
-    }
+  const multiplicator = distToRail > 0 ? 1 : -1;
+  force = (-getBaseLog(range, Math.abs(distToRail)) + 1) * maxForce
+    * multiplicator;
+  if (distToRail > -1 && distToRail < 1) {
+    force = 0;
   }
   return force;
 };
 
-// behave like a "masculine" bump with a sharp edge at the position of the rail but with a linear incline
-const linearInclineRails = function(x, maxForce, range) {
-  let force = 0;
-  const multiplicator = x < 0 ? -1 : 1;
-  if (x > - range && x < range) {
-    force = multiplicator * (-multiplicator * (maxForce/range)*x + maxForce);
-  }
-
-  return force;
+// behave like a "male" bump with a sharp edge at the position of the rail
+// but with a linear incline
+const linearInclineRails = function(distToRail, maxForce, range) {
+  const multiplicator = distToRail > 0 ? 1 : -1;
+  return -1 * (multiplicator * (-(maxForce / range) * Math.abs(distToRail))
+    + (multiplicator * maxForce));
 };
 
-// behave like a "masculine" bump with a sharp edge at the position of the rail without incline of force
-const squareRails = function(x, force, range) {
-  const multiplicator = x < 0 ? -1 : 1;
-  if (x > - range && x < range) {
-    return multiplicator * force;
+// behave like a "male" bump with a sharp edge at the position of the rail
+// without incline of force
+const squareRails = function(distToRail, force, range) {
+  const multiplicator = distToRail < 0 ? 1 : -1;
+  return multiplicator * force;
+};
+
+// a "female" bump that physically has the shape of a half pipe (or a "u") and
+// keeps the user between 2 bumps
+const halfpipeBumb = function(distToRail, maxForce, range) {
+  return Math.pow(-distToRail, 3) * maxForce / Math.pow(range, 3);
+};
+
+// for "male" bump rails that should not push the
+// user away once they overcame the bump
+const softTail = function(distToRail, maxForce, range) {
+  // x > 0 means that we pushed to the right and hence need negative force
+  const multiplicator = distToRail >= 0 ? -1 : 1;
+  // the m in the formula m * x^2 + n
+  if (Math.abs(distToRail) < range / 2) {
+    const m = -multiplicator * maxForce / Math.pow(range, 2);
+    return - (m * Math.pow(distToRail, 2) + multiplicator * maxForce);
   } else {
     return 0;
   }
 };
 
-// a "feminine" bump that physically has the shape of a half pipe (or a "u") and keeps the user between 2 bumps
-const halfpipeBumb = function(x, maxForce, range) {
-  if (x > - range && x < range) {
-    return Math.pow(-x, 3)*maxForce/Math.pow(range, 3);
-  }
-  return 0;
+// trigger the current rail function using keyboard presses
+const railFunctions = {
+  '1': negativeLogarithmicBump,
+  '2': linearInclineRails,
+  '3': negativeLogarithmicRails,
+  '4': halfpipeBumb,
+  '5': squareRails
 };
 
-// for "masculine" bump rails that should not push the user away once they overcame the bump
-const softTail = function(x, maxForce, range) {
-  // x > 0 means that we pushed to the right and hence need negative force
-  const multiplicator = x >= 0 ? -1 : 1;
+let railFunc = negativeLogarithmicBump;
+let softTailActive = false;
 
-  // the m in the formula m * x^2 + n
-  const m = -multiplicator * maxForce/Math.pow(range, 2);
-  if (Math.abs(x) < range) {
-    return (m * Math.pow(x, 2) + multiplicator * maxForce);
+stdin.on('data', function(key) {
+  // ctrl-c ( end of text )
+  if (key === '\u0003') {
+    process.exit();
   }
-  return 0;
+  if (key === 'x') {
+    softTailActive = !softTailActive;
+    console.log('Soft tail active', softTailActive);
+  }
+  if (key in railFunctions) {
+    railFunc = railFunctions[key];
+  }
+  console.log(railFunc);
+});
+
+
+const getDistToRail = function(position, rail) {
+  const sideOfRail = (position.x - rail[0].x) * (rail[1].y - rail[0].y) -
+  (position.y - rail[0].y) * (rail[1].x - rail[0].x) > 0 ? 1 : -1;
+  return sideOfRail * pDistance(position.x, position.y,
+      rail[0].x, rail[0].y,
+      rail[1].x, rail[1].y);
 };
 
 const rails = function(position, lastPosition) {
-  let force = 0;
-  const maxForce = 30;
-  const range = 10;
-
-  // add different kinds of bumps to test various haptic rails
-  force = negativeLogarithmicRails(position.x, maxForce, range);
-  // force = linearInclineRails(position.x, maxForce, range);
-  // force = squareRails(position.x, 20, 5);
-
-  // this means that we move away from/already passed the bump
-  // add soft tail (decline of force according to quadratic/negative quadratic curve)
-  if (lastPosition && (position.x-lastPosition.x)/position.x>0) {
-    force = softTail(position.x, maxForce, range/2);
+  const rails = [
+    [new Vector(-40, -150), new Vector(-5, -70)],
+    [new Vector(40, -150), new Vector(5, -70)]];
+  const force = new Vector(0, 0, NaN);
+  const forceFactor = 0.01;
+  const maxForce = 25; // railFunc === negativeLogarithmicRails ? 15 : 5;
+  const range = 15;
+  for (let r = 0; r < rails.length; r++) {
+    const rail = rails[r];
+    const railVec = rail[1].subtract(rail[0]);
+    const railOrthogonal = new Vector(-railVec.y, railVec.x);
+    const railOrthogonalDir = railOrthogonal.normalized();
+    const distToRail = getDistToRail(position, rail);
+    if (Math.abs(distToRail) < range) {
+      let railForce = railFunc(distToRail, maxForce, range);
+      console.log('Force', railForce);
+      if (softTailActive && lastPosition) {
+        const lastDistToRail = getDistToRail(lastPosition, rail);
+        if ((distToRail - lastDistToRail) / distToRail > 0) {
+          railForce = softTail(distToRail, maxForce, range / 2);
+          console.log('Force after soft tail application', railForce);
+        }
+      }
+      railForce *= forceFactor;
+      const forceVec = railOrthogonalDir.scale(railForce);
+      force.add(forceVec);
+    }
   }
 
-  // force = halfpipeBumb(position.x, 20, 3);
-  console.log(force);
-
-  return new Vector(force*0.01, 0, NaN);
+  return force;
 };
 
 function start() {
+  // render small maze (kinda like doom level 1)
+  const leftWallPart = device.addHapticObject(
+      new Vector(-75, -50));
+  leftWallPart.addComponent(
+      new BoxCollider(new Vector(120, 10)));
+  const leftMiddleWall = device.addHapticObject(
+      new Vector(-25, -60));
+  leftMiddleWall.addComponent(
+      new BoxCollider(new Vector(10, 20)));
+  const rightWallPart = device.addHapticObject(
+      new Vector(75, -50));
+  rightWallPart.addComponent(
+      new BoxCollider(new Vector(120, 10)));
+  const rightMiddleWall = device.addHapticObject(
+      new Vector(25, -60));
+  rightMiddleWall.addComponent(
+      new BoxCollider(new Vector(10, 20)));
+
   const hapticObject = device.addHapticObject(new Vector(0, -100));
-  hapticObject.addComponent(new BoxForcefield(new Vector(400, 200), rails));
+  hapticObject.addComponent(new BoxForcefield(new Vector(200, 100), rails));
 }
