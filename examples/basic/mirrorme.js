318f7bd0726125cb36a0a0e1a3567e6ca1cb465d
Jotaro <jojojoe77@gmail.com>
2020-06-17 12:34:44 +0200

    [WIP] rotation might not match.

---
+++ examples/basic/mirrorme.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const {Broker, Vector} = require('../..');
+
+Broker.on('devicesChanged', function(devices, attached, detached) {
+  for (const device of devices) {
+    if (device) {
+      device.on('handleMoved', function(index, pos) {
+        if (index==0) {
+          device.moveHandleTo(1, new Vector(pos.x, pos.y, -pos.r));
+          // device.rotateHandleTo(1, pos.r);
+        }
+      });
+    }
+  }
+});
