4b3499a8334d61eddda40a21843ab78f89a055dc
Julius Rudolph <julius.rudolph@student.hpi.de>
2020-09-04 16:37:06 +0200

    clean up and add comment for rail

--- firmware/include/physics/rail.hpp
+++ firmware/include/physics/rail.hpp
@@ -6,7 +6,7 @@ class Rail : public Obstacle
 {
 
 private:
-    uint32_t displacement;
+    uint32_t displacement; // orthogonal distance to the rail until where the user still feels the applied force of the rail
 
 public:
     Rail(std::vector<Vector2D> points, double displacement);
