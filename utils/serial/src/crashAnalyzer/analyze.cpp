9c8100c515df4f736914981cb4dd7b26b4255eb1
Lukas Wagner <lukas.wagner@student.hpi.de>
2021-03-07 19:31:24 +0100

    fix debugging on windows

--- utils/serial/src/crashAnalyzer/analyze.cpp
+++ utils/serial/src/crashAnalyzer/analyze.cpp
@@ -8,14 +8,25 @@
 #include <sstream>
 #include "libInterface.hpp"
 
+#ifdef WINDOWS
+#include <stdio.h>
+#define popen _popen
+#define pclose _pclose
+#endif
+
+#ifdef __APPLE__
+#define popen popen
+#define pclose pclose
+#endif
+
 const std::string CrashAnalyzer::c_rebootString = "Rebooting...";
 const std::string CrashAnalyzer::c_backtraceString = "Backtrace:";
 
 bool CrashAnalyzer::findString(
-        uint16_t startOffset,
-        uint16_t endOffset,
-        const std::string string,
-        uint16_t& foundOffset)
+    uint16_t startOffset,
+    uint16_t endOffset,
+    const std::string string,
+    uint16_t &foundOffset)
 {
     const auto length = string.length();
     int32_t index = length - 1;
@@ -23,7 +34,7 @@ bool CrashAnalyzer::findString(
 
     while (index > -1 && offset <= endOffset)
     {
-        if(getChar(offset) == string.at(index))
+        if (getChar(offset) == string.at(index))
         {
             index--;
         }
@@ -34,7 +45,7 @@ bool CrashAnalyzer::findString(
         offset++;
     }
 
-    if(index == -1)
+    if (index == -1)
     {
         foundOffset = offset;
         return true;
@@ -47,7 +58,7 @@ std::vector<std::string> CrashAnalyzer::getBacktraceAddresses(
     uint16_t startOffset, uint16_t endOffset)
 {
     std::string data(startOffset - endOffset + 1, 'a');
-    for(auto i = startOffset; i >= endOffset; --i)
+    for (auto i = startOffset; i >= endOffset; --i)
     {
         data.at(startOffset - i) = getChar(i);
     }
@@ -56,7 +67,7 @@ std::vector<std::string> CrashAnalyzer::getBacktraceAddresses(
     auto end = std::sregex_iterator();
 
     std::vector<std::string> result;
-    while(it != end)
+    while (it != end)
     {
         result.push_back((*it++).str(1));
     }
@@ -64,48 +75,45 @@ std::vector<std::string> CrashAnalyzer::getBacktraceAddresses(
     return result;
 }
 
-char * exec(const char* cmd) {
-    #ifdef WINDOWS
-    #define popen _popen
-    #define pclose _pclose
-    #endif
-    loggingHandler((char*)cmd);
-    #ifdef __APPLE__
-    #define popen popen
-    #define pclose pclose
-    #endif
+char *exec(const char *cmd)
+{
+    loggingHandler((char *)cmd);
 
     std::array<char, 1024> buffer;
     std::string result;
     std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
-    if (!pipe) {
+    if (!pipe)
+    {
         loggingHandler("Popen failed");
         return "popen() failed!";
     }
-    while (fgets(buffer.data(), buffer.size(), pipe.get()) != NULL) {
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != NULL)
+    {
         loggingHandler(buffer.data());
         //result += buffer.data();
     }
     char *cstr = new char[result.length() + 1];
     strcpy(cstr, result.c_str());
     // do stuff
-    delete [] cstr;
+    delete[] cstr;
     return cstr;
 }
 
 void CrashAnalyzer::addr2line(std::vector<std::string> addresses)
 {
-    std::cout << std::endl << "===== STACKTRACE BEGIN =====" << std::endl;
+    std::cout << std::endl
+              << "===== STACKTRACE BEGIN =====" << std::endl;
 
     loggingHandler("===== STACKTRACE BEGIN =====");
-    #ifdef __APPLE__
-    #define ADDR2LINE_PATH "~/.platformio/packages/toolchain-xtensa32/bin/xtensa-esp32-elf-addr2line"
-    #endif
-    #ifdef ADDR2LINE_PATH
+#ifdef __APPLE__
+#define ADDR2LINE_PATH "~/.platformio/packages/toolchain-xtensa32/bin/xtensa-esp32-elf-addr2line"
+#define FIRMWARE_PATH "/Users/julio/Documents/Uni/5_Master/HCI_Project_Seminar/dualpantoframework/firmware/.pio/build/esp32dev/firmware.elf"
+#endif
+#ifdef ADDR2LINE_PATH
     std::ostringstream command;
     command
         << ADDR2LINE_PATH
-        << " -e /Users/julio/Documents/Uni/5_Master/HCI_Project_Seminar/dualpantoframework/firmware/.pio/build/esp32dev/firmware.elf"
+        << " -e " << FIRMWARE_PATH
         << " -fpCis"; // see https://linux.die.net/man/1/addr2line
     for (const auto &address : addresses)
     {
@@ -118,13 +126,13 @@ void CrashAnalyzer::addr2line(std::vector<std::string> addresses)
     std::ostringstream out;
     out << "Stacktrace (most recent call first):" << std::endl
         << result;
-    #else
+#else
     loggingHandler("Path to addr2line executable not set. Can't analyze stacktrace.");
     std::cout
         << "Path to addr2line executable not set. Can't analyze stacktrace."
         << std::endl;
 
-    #endif
+#endif
 
     std::cout << "====== STACKTRACE END ======" << std::endl;
 }
@@ -132,21 +140,21 @@ void CrashAnalyzer::addr2line(std::vector<std::string> addresses)
 void CrashAnalyzer::checkOutput()
 {
     uint16_t rebootOffset;
-    if(!findString(
-        0,
-        c_rebootString.length(),
-        c_rebootString,
-        rebootOffset))
+    if (!findString(
+            0,
+            c_rebootString.length(),
+            c_rebootString,
+            rebootOffset))
     {
         return;
     }
 
     uint16_t backtraceOffset;
-    if(!findString(
-        rebootOffset,
-        s_length,
-        c_backtraceString,
-        backtraceOffset))
+    if (!findString(
+            rebootOffset,
+            s_length,
+            c_backtraceString,
+            backtraceOffset))
     {
         std::cout << "Reboot detected, but no backtrace found." << std::endl;
         return;
@@ -154,16 +162,18 @@ void CrashAnalyzer::checkOutput()
     char out[c_bufferLength + 1];
     std::memcpy(out, s_buffer, c_bufferLength);
     out[c_bufferLength] = '\0';
-    for (int i=0;i<c_bufferLength;i++){
+    for (int i = 0; i < c_bufferLength; i++)
+    {
         char x = s_buffer[i];
-        if (x < 32 || x > 126){
+        if (x < 32 || x > 126)
+        {
             // space
             x = 32;
         }
-        out[i]=x;
+        out[i] = x;
     }
     loggingHandler("ERROR!");
-    loggingHandler(out); 
+    loggingHandler(out);
     auto addresses = getBacktraceAddresses(
         backtraceOffset - c_backtraceString.length() - 1,
         rebootOffset);
