459ba57a0e9ca96811b159a1658ff298f873861f
Julius Rudolph <julius.rudolph@student.hpi.de>
2021-09-08 14:29:24 +0200

    override the new operator

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -5,29 +5,42 @@
 #include "tasks/taskRegistry.hpp"
 #include "utils/serial.hpp"
 
+//new.cpp
+#include <cstdlib>
+#include <new>
+
+void *operator new(size_t size) noexcept { return ps_malloc(size); }
+void operator delete(void *p) noexcept { free(p); }
+void *operator new[](size_t size) noexcept { return operator new(size); }
+void operator delete[](void *p) noexcept { operator delete(p); }
+void *operator new(size_t size, std::nothrow_t) noexcept { return operator new(size); }
+void operator delete(void *p, std::nothrow_t) noexcept { operator delete(p); }
+void *operator new[](size_t size, std::nothrow_t) noexcept { return operator new(size); }
+void operator delete[](void *p, std::nothrow_t) noexcept { operator delete(p); }
+
 void setup()
 {
     DPSerial::init();
 
     DPSerial::sendInstantDebugLog("========== START ==========");
 
-    Tasks.emplace(
-        std::piecewise_construct,
-        std::forward_as_tuple("I/O"), 
-        std::forward_as_tuple(&ioSetup, &ioLoop, "I/O", 0));
-    Tasks.at("I/O").run();
-    Tasks.at("I/O").setLogFps();
-    Tasks.emplace(
-        std::piecewise_construct,
-        std::forward_as_tuple("Physics"),
-        std::forward_as_tuple(&physicsSetup, &physicsLoop, "Physics", 1));
-    Tasks.at("Physics").run();
+    // Tasks.emplace(
+    //     std::piecewise_construct,
+    //     std::forward_as_tuple("I/O"), 
+    //     std::forward_as_tuple(&ioSetup, &ioLoop, "I/O", 0));
+    // Tasks.at("I/O").run();
+    // Tasks.at("I/O").setLogFps();
+    // Tasks.emplace(
+    //     std::piecewise_construct,
+    //     std::forward_as_tuple("Physics"),
+    //     std::forward_as_tuple(&physicsSetup, &physicsLoop, "Physics", 1));
+    // Tasks.at("Physics").run();
     
-    TaskHandle_t defaultTask = xTaskGetCurrentTaskHandle();
-    DPSerial::sendInstantDebugLog("default task handle is %i", defaultTask);
-    vTaskSuspend(NULL);
-    taskYIELD();
-    DPSerial::sendInstantDebugLog("setup - this should not be printed");
+    // TaskHandle_t defaultTask = xTaskGetCurrentTaskHandle();
+    // DPSerial::sendInstantDebugLog("default task handle is %i", defaultTask);
+    // vTaskSuspend(NULL);
+    // taskYIELD();
+    // DPSerial::sendInstantDebugLog("setup - this should not be printed");
 }
 
 void loop()
