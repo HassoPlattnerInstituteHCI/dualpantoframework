a148e4250ca0bebf34e9d02fa5fd64b9771d1852
Julius Rudolph <julius.rudolph@student.hpi.de>
2021-11-08 18:59:25 +0100

    add custom SPI ram allocator

---
+++ firmware/src/utils/spiramallocator.cpp
@@ -0,0 +1,15 @@
+#include "esp_heap_caps.h"
+
+struct SpiRamAllocator {
+  void* allocate(size_t size) {
+    return heap_caps_malloc(size, MALLOC_CAP_SPIRAM);
+  }
+
+  void deallocate(void* pointer) {
+    heap_caps_free(pointer);
+  }
+
+  void* reallocate(void* ptr, size_t new_size) {
+    return heap_caps_realloc(ptr, new_size, MALLOC_CAP_SPIRAM);
+  }
+};
